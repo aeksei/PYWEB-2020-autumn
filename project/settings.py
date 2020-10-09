7aa6d36ae820c208a06441cc7eb62a9713c7e128
aeksei <aeksei94@gmail.com>
2020-10-09 22:58:48 +0500

    add static root dir

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'true'
 
-print(os.getenv('ALLOWED_HOSTS'))
 ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS').split(',')]
 
 
