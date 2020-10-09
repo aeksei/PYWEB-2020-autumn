13a3d04a46be728d12bebe2e83189751d5909770
aeksei <aeksei94@gmail.com>
2020-10-09 22:24:14 +0500

    add debug print

--- project/settings.py
+++ project/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'true'
 
+print(os.getenv('ALLOWED_HOSTS'))
 ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS').split(',')]
 
 
